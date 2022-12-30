f240274813bdc26324d0377e80b052755ff4158d
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-12-30 16:06:22 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,13 +1,6 @@
-// This file was added by layer0 init.
-// You should commit this file to source control.
-
-import { Router } from '@layer0/core/router'
-
-// const ONE_HOUR = 60 * 60
-// const ONE_DAY = 24 * ONE_HOUR
+import { Router } from '@edgio/core/router'
 
 export default new Router()
-  // send any unmatched request to origin
   .fallback(({ serveStatic, cache, compute }) => {
     cache({
       edge: {
@@ -17,7 +10,7 @@ export default new Router()
     })
     serveStatic('index.html', {
       onNotFound: async () => await compute((req, res) => {
-        res.body= new Date().toString()
+        res.body = new Date().toString()
         res.statusCode= 200
         res.statusMessage = 'OK'
       })
